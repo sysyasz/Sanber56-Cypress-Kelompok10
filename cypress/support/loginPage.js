e16232af96f70351688cdcd56ae6302646436caf
sysyasz <syasyasz07@gmail.com>
2024-05-21 21:28:13 +0700

    Tambah test case login

--- cypress/support/loginPage.js
+++ cypress/support/loginPage.js
@@ -1,19 +1,21 @@
 class LoginPage {
-
-    visit() {
-  cy.visit('https://magento.softwaretestingboard.com/')
+  visit() {
+cy.visit('https://magento.softwaretestingboard.com/customer/account/login')
 }
 
 fillEmail(email) {
-  cy.get('input[name="coba.test@gmail.com"]').type(email)
+// cy.get('input[name="coba.test@gmail.com"]').type(email)
+cy.get('#email').type(email)
 }
 
 fillPassword(password) {
-  cy.get('input[name="password123#"]').type(password)
+// cy.get('input[name="password123#"]').type(password)
+cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type(password)
 }
 
 clickLoginButton() {
-  cy.get('button[type="submit"]').click()
+// cy.get('button[type="submit"]').click()
+cy.get('#send2').click()
 }}
 
 export default new LoginPage()
